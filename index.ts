a3dc84c21e20f7811b53a99382efa137b6958bc4
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2023-08-16 23:25:00 +0900

    [wip]

--- index.ts
+++ index.ts
@@ -1,32 +1,58 @@
+import * as dotenv from "dotenv";
 import * as prompts from "prompts";
 import * as readline from "node:readline/promises";
-import { stdin as input, stdout as output } from "node:process";
-import { readdir } from "node:fs/promises";
+import { exit, stdin as input, stdout as output } from "node:process";
+import { readFile, readdir } from "node:fs/promises";
+import { mkdir } from "node:fs";
 
-prompts({
-    type: "number"
-    , name: "value" 
-    , message: "Select a project:"
-    , validate: (value: number) => value < 18 ? `Nightclub is 18+ only` : true
-})
-    .then((value: prompts.Answers<"value">) => {
-        console.log(value);
-    });
-  
+dotenv.config();
 
-// const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface({ input, output });
 
-// readdir("/Users/yusuke/Workspaces/", { withFileTypes : true })
-//     .then((dirents) => {
-//         dirents.forEach((dirent) => {
-//             if (!dirent.isDirectory || dirent.name.startsWith(".")) return;
-//             console.log(dirent.name);
-//         });
-//     });
+const path = process.env.WORKSPACE_PATH;
 
-// rl.question("Choose a project to setup?\n")
-//     .then((answer) => {
-//         console.log(`Thank you for your valuable feedback: ${answer}`);
+if (path === undefined) {
+    exit(1);
+}
 
-//     })
-//     .finally(() => rl.close());
+readdir(path, { withFileTypes : true })
+    .then((dirents) => {
+        return dirents.filter(d => d.isDirectory() && !d.name.startsWith("."))
+            .map(d => ({ title: d.name, value: d.name }));
+    })
+    .then((choices: prompts.Choice[]) => {
+        const questions = [
+            {
+                type: "select"
+                , name: "project"
+                , message: "Select a project:"
+                , choices
+            }
+        ] as prompts.PromptObject[];
+        return prompts(questions);  
+    })
+    .then((answers: prompts.Answers<string>) => {
+        const projectPath = `${ path }/${ answers.project }`;
+        
+        // package.json
+        readFile(`${ projectPath }/package.json`, "utf8")
+            .then((data) => {
+                const packageJson = JSON.parse(data);
+                console.log("packageJson", packageJson);
+            });
+        
+        // ディレクトリの作成
+        mkdir(`${ projectPath }/src/client`, { recursive: true }, (err) => {
+            if (err) { throw err; }
+            console.log('testディレクトリが作成されました');
+        });
+        mkdir(`${ projectPath }/src/server`, { recursive: true }, (err) => {
+            if (err) { throw err; }
+            console.log('testディレクトリが作成されました');
+        });
+        mkdir(`${ projectPath }/src/shared`, { recursive: true }, (err) => {
+            if (err) { throw err; }
+            console.log('testディレクトリが作成されました');
+        });
+    })
+    .finally(() => rl.close());
