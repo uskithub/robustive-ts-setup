95885f45bb3a547a9fe334b00de4cb9944f955b1
Yusuke SAITO <yusuke.saito@jibunstyle.com>
2024-02-29 15:56:00 +0900

    Added vite-tsconfig-paths

--- template/vite.config.ts
+++ template/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
-import * as path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,23 +11,9 @@ export default defineConfig({
         , vuetify({
             autoImport: true
         })
+        , tsconfigPaths()
     ]
     , define: { "process.env": {} }
-    , resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src/")
-            , "@client": path.resolve(__dirname, "src/client")
-            , "@server": path.resolve(__dirname, "src/server")
-            , "@shared": path.resolve(__dirname, "src/shared")
-            , "@views": path.resolve(__dirname, "src/client/service/presentation/views")
-            , "@domain": path.resolve(__dirname, "src/shared/service/domain")
-            , "@interfaces": path.resolve(__dirname, "src/shared/service/domain/interfaces")
-            , "@actors": path.resolve(__dirname, "src/shared/service/application/actors")
-            , "@usecases": path.resolve(__dirname, "src/shared/service/application/usecases")
-            , find: "./runtimeConfig"
-            , replacement: "./runtimeConfig.browser"
-        }
-    }
     , server: {
         port : 3001
     }
